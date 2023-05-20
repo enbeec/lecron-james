742a6e2e14f5d837468d9de320a875bc4fbb709c
Val Currie <contact@valcurrie.com>
2023-05-20 17:30:36 -0500

    fix deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,15 +7,13 @@ expose=()
 
 # build exposed key array
 for i in "${!env[@]}"; do
-    for i in "${!env[@]}"; do
-        key="${env[$i]%=*}"     # trim =* from end
+    key="${env[$i]%=*}"     # trim =* from end
 
-        # if key ends in "KEY" it is a secret, otherwise a plain env var
-        if [[ "$key" == *KEY ]]; then
-            secret_key=${key,,}
-            expose+=(-e "${key}=@${secret_key}")
-        fi
-    done
+    # if key ends in "KEY" it is a secret, otherwise a plain env var
+    if [[ "$key" == *KEY ]]; then
+        secret_key=${key,,}
+        expose+=(-e "${key}=@${secret_key}")
+    fi
 done
 
 vercel --prod "${expose[@]}"
